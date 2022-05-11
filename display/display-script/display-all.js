f5b9a8944d4dfe34fa2b618c7a2ca51d81f879fa
AshtonMar <ashtonmartingoliath@gmail.com>
2022-05-11 15:56:27 +0200

    New functionlity added

--- display/display-script/display-all.js
+++ display/display-script/display-all.js
@@ -1,24 +1,24 @@
 function userInfo() {
 	fetch(`http://127.0.0.1:5000/view_yp_profiles/`, {
-        method: "GET",
-        headers: { "Content-Type": "application/json" },
-    })
-        .then((response) => response.json())
-        .then((data) => {
+		method: "GET",
+		headers: { "Content-Type": "application/json" },
+	})
+		.then((response) => response.json())
+		.then((data) => {
 			let yp = data.yp_data;
 
-			if(yp.length === 0){
+			if (yp.length === 0) {
 				alert("There is no data to view")
-			}else{
-				for(i of yp){
+			} else {
+				for (i of yp) {
 					console.log(i);
 					ypProfileCard(i)
 				}
 			}
 		})
-			.catch((error) => {
-            console.log(error);
-        });
+		.catch((error) => {
+			console.log(error);
+		});
 }
 
 function ypProfileCard(info) {
@@ -26,7 +26,7 @@ function ypProfileCard(info) {
 	let ypInfoDisplay = document.querySelector("#card-view");
 
 	let ypCard = `
-	<div id="${info["yp_id"]}" class="info-card">
+	<div id="${info["yp_id"]}" class="${info["full-name"]} info-card">
 		<img src="${info["profile_image"]}" alt="person-image" class="person-image" />
 		<div id="yp-info" class="persons-info">
 			<li><a class="persons-name">${info["full_name"]}</a></li>
@@ -37,28 +37,31 @@ function ypProfileCard(info) {
 	`;
 
 	ypInfoDisplay.innerHTML += ypCard
-  }
+}
 
 userInfo()
 
-function getAge(birthdayDate){
+function getAge(birthdayDate) {
 	let today = new Date();
-    let birthDate = new Date(birthdayDate);
+	let birthDate = new Date(birthdayDate);
 
-    let age = today.getFullYear() - birthDate.getFullYear();
-    let m = today.getMonth() - birthDate.getMonth();
-    
-	if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) 
-    {
-        age--;
-    }
-	
+	let age = today.getFullYear() - birthDate.getFullYear();
+	let m = today.getMonth() - birthDate.getMonth();
+
+	if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+		age--;
+	}
+
 	return age
 }
 
 document.getElementById("search-bar").addEventListener("keyup", () => {
 	let input = document.getElementById("search-bar");
+	let cardValue = document.getElementsByClassName("info-card")
 
+	console.log(cardValue.className);
 	console.log(input.value);
+
+
 
 })
