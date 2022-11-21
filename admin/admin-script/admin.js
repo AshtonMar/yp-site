fbdead14aa5336edca7c708b1fad48a4010487a4
AshtonMar <ashtonmartingoliath@gmail.com>
2022-11-21 08:22:09 +0200

    Updated admin functionality

--- admin/admin-script/admin.js
+++ admin/admin-script/admin.js
@@ -6,32 +6,76 @@ function fetchData() {
 		.then((response) => response.json())
 		.then(data => {
 			data = data.yp_data;
-			createYpDataRow(data)
+			let table_headings = Object.keys(data[0]);
+
+			const admin_table_created = createAdminTable(data, table_headings);
+			if (admin_table_created["created"])
+				adminControls(admin_table_created["info"]);
 		})
 		.catch((error) => {
-			alert(error);
+			console.log(error);
 		});
 }
 
 fetchData();
 
-function createYpDataRow(youngPeople) {
-	const table = document.getElementById("young-people-table");
+function createAdminTable(yp_information, heading_names) {
 
-	youngPeople.forEach(youngPerson => {
-		const ypRow = `
-		<tr class="yp-row admin-control" >
-			<td class="info-column">
-			<i class="fa-solid fa-pen-to-square"></i>
-				<button>Delete</button>
-			</td>
-			<td class="id-column hidden">${youngPerson["yp_id"]}</td>
-			<td class="info-column"><img class="table-image" src="${youngPerson["profile_image"]}" alt="yp-image"></td>
-			<td class="info-column">${youngPerson["full_name"]}</td>
-			<td class="info-column">${youngPerson["birthday"]}</td>
-			<td class="info-column">${youngPerson["phone_number"]}</td>
-		</tr >`;
+	if (yp_information == undefined || heading_names == undefined)
+		return { "created": false };
 
-		table.innerHTML += ypRow;
-	});
+	const heading_columns = document.getElementById("table-headings");
+	const information_columns = document.getElementById("table-information");
+
+	for (const heading_name of heading_names) {
+		if (heading_name !== "profile_image" && heading_name !== "yp_id") {
+			const heading_column_structure = `<div class="table-heading">${heading_name}</div>`;
+
+			heading_columns.innerHTML += heading_column_structure;
+		}
+	}
+
+	for (const yp_person of yp_information) {
+		const yp_person_row = `<div id="table-row-${yp_person["yp_id"]}" class="table-row"></div>`;
+
+		information_columns.innerHTML += yp_person_row;
+
+		const yp_person_row_column = document.getElementById(`table-row-${yp_person["yp_id"]}`);
+
+		for (const key of heading_names) {
+			if (key !== "profile_image" && key !== "yp_id") {
+				const yp_person_columns = `<div class="table-column">${yp_person[key]}</div>`;
+
+				yp_person_row_column.innerHTML += yp_person_columns;
+			}
+		}
+	}
+
+	return { "created": true, "info": yp_information };
+}
+
+
+function adminControls(yp_information) {
+	// const update_btn = document.getElementById();
+	// const delete_btn = document.getElementById();
+	// const exit_btn = document.getElementById();
+
+	const data_rows = document.getElementsByClassName("table-row");
+
+	for (const row of data_rows) {
+		row.addEventListener("click", () => {
+			let row_id = row.id.split("-");
+			let info_id = parseInt(row_id[row_id.length - 1]);
+
+			for (const yp_info of yp_information) {
+				if (info_id === yp_info["yp_id"]) {
+					row.className = "highlighted_row";
+				} else {
+					row.className = "table-row";
+				}
+			}
+
+		})
+	}
+
 }
