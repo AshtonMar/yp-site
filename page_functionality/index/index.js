e9117e7b688ca762d4004d9052a076cb5ab10308
AshtonMar <ashtonmartingoliath@gmail.com>
2023-01-23 14:51:39 +0200

    Main page fix

--- page_functionality/index/index.js
+++ page_functionality/index/index.js
@@ -1,104 +1,97 @@
-let userNames = [];
-let signInBtn = document.getElementById("signin-btn");
-let signUpBtn = document.getElementById("signup-btn");
+let usernames = [];
+let signin_btn = document.getElementById('signin-btn');
+let signup_btn = document.getElementById('signup-btn');
 
-signInBtn.addEventListener("click", () => {
-	createForms(signInBtn.value);
+signin_btn.addEventListener('click', () => {
+	createForms(signin_btn.value);
 });
 
-signUpBtn.addEventListener("click", () => {
-	createForms(signUpBtn.value);
+signup_btn.addEventListener('click', () => {
+	createForms(signup_btn.value);
 });
 
 function createForms(form) {
-	let formSection = document.getElementById("form-display");
-	let currentForm = "";
+	let form_section = document.getElementById('form-display');
+	let current_form = "";
 
 	const forms = {
-		adminForm: `
-			<form id="signin-form" class="form" action="#">
-				<input required autocomplete="off" id="signin-username" type="text" placeholder="Username" />
-				<div class="view-password">
-					<input required autocomplete="off" id="signup-password" type="password" placeholder="Password" />
-					<i id="toggle-password" class="fa-solid fa-eye-low-vision"></i>
+		admin_form: `
+			<form id='signin-form' class='form' action='#'>
+				<input required autocomplete='off' id='signin-username' type='text' placeholder='Username' />
+				<div class='view-password'>
+					<input required autocomplete='off' id='signup-password' type='password' placeholder='Password' />
+					<i id='toggle-password' class='fa-solid fa-eye-low-vision'></i>
 				</div>
-				<button id="admin-submit-btn" type="submit" value="submit">Sign In</button>
+				<button id='admin-submit-btn' type='submit' value='submit'>Sign In</button>
 			</form>
 		`,
-		registrationForm: `
-			<form id="signup-form" class="form" action="#">
-				<input autocomplete="off" id="user-fullname" required type="text" placeholder="Full Name" />
-				<input autocomplete="off" id="user-phone-number" required type="number" max-length="10" placeholder="Phone Number" />
-				<input autocomplete="off" id="user-image" type="file" accept="image/*" placeholder="Personal Image" />
-				<img id="output" width="100%"/>
-				<div class="birthdate-entry">
-					<input autocomplete="off" id="month" class="date-input input" required type="number" placeholder="mm" maxlength="2" />
-					<label class="divider">/</label>
-					<input autocomplete="off" id="day" class="date-input input" required type="number" placeholder="dd" maxlength="2" />
-					<label class="divider">/</label>
-					<input autocomplete="off" id="year" class="date-input input" required type="number" placeholder="yyyy" maxlength="4" />
+		registration_form: `
+			<form id='signup-form' class='form' action='#'>
+				<input autocomplete='off' id='user-fullname' required type='text' placeholder='Full Name' />
+				<input autocomplete='off' id='user-phone-number' required type='number' max-length='10' placeholder='Phone Number' />
+				<input autocomplete='off' id='user-image' type='file' accept='image/*' placeholder='Personal Image' />
+				<img id='output' width='100%'/>
+				<div class='birthdate-entry'>
+					<input autocomplete='off' id='month' class='date-input input' required type='number' placeholder='mm' maxlength='2' />
+					<label class='divider'>/</label>
+					<input autocomplete='off' id='day' class='date-input input' required type='number' placeholder='dd' maxlength='2' />
+					<label class='divider'>/</label>
+					<input autocomplete='off' id='year' class='date-input input' required type='number' placeholder='yyyy' maxlength='4' />
 				</div>
-				<button id="user-submit-btn">Register User</button>
+				<button id='user-submit-btn'>Register User</button>
 			</form>
 		`
 	}
 
-	if (form === "signin-form") {
-		currentForm = forms["adminForm"];
+	if (form === 'signin-form') {
+		current_form = forms['admin_form'];
 
-		signInBtn.disabled = true;
-		signUpBtn.disabled = false;
+		signin_btn.disabled = true;
+		signup_btn.disabled = false;
+	} else if (form === 'signup-form') {
+		current_form = forms['registration_form'];
 
-		document.body.style = `
-			background-image: url("../../global_use/global_images/background.png"), linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3));
-			background-blend-mode: overlay;
-			background-position: center;
-			background-size: cover;
-		`;
-	} else if (form === "signup-form") {
-		currentForm = forms["registrationForm"];
-
-		signUpBtn.disabled = true;
-		signInBtn.disabled = false;
-
-		document.body.style = `
-			background-image: url("../../global_use/global_images/background.png"), linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3));
-			background-blend-mode: overlay;
-			background-position: center;
-			background-size: cover;
-		`;
+		signup_btn.disabled = true;
+		signin_btn.disabled = false;
 	}
 
-	formSection.innerHTML = currentForm;
+	document.body.style = `
+		background-image: url('../../global_use/global_images/background.png'), linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3));
+		background-blend-mode: overlay;
+		background-position: center;
+		background-size: cover;
+	`;
 
-	if (currentForm === forms["adminForm"]) {
-		document.getElementById("signin-form").addEventListener("submit", userInfo);
+	form_section.innerHTML = current_form;
 
-		document.getElementById("toggle-password").addEventListener("click", () => {
-			let password = document.getElementById("signup-password");
+	if (current_form === forms['admin_form']) {
+		document.getElementById('signin-form').addEventListener('submit', userInfo);
 
-			if (password.type === "password") {
-				password.type = "text";
-				document.getElementById("toggle-password").className = "fa-regular fa-eye";
+		document.getElementById('toggle-password').addEventListener('click', () => {
+			let password = document.getElementById('signup-password');
+
+			if (password.type === 'password') {
+				password.type = 'text';
+				document.getElementById('toggle-password').className = 'fa-regular fa-eye';
 
 			} else {
-				password.type = "password";
-				document.getElementById("toggle-password").className = "fa-solid fa-eye-low-vision";
+				password.type = 'password';
+				document.getElementById('toggle-password').className = 'fa-solid fa-eye-low-vision';
 
 			}
 		});
 
 	} else {
-		let imageInput = document.getElementById("user-image");
-		let image = document.getElementById("output");
-		let inputValueCheck = ["", null, "undefined"];
+		let image_input = document.getElementById('user-image');
+		let image = document.getElementById('output');
+		let input_value_check = ["", null, 'undefined'];
 
-		imageInput.addEventListener("change", function () {
+		image_input.addEventListener('change', function () {
 
-			if (inputValueCheck.includes(imageInput.value) === false) {
+			if (input_value_check.includes(image_input.value) === false) {
 				const reader = new FileReader(); // Converts the file into a data url(contains info of the file)
 
-				reader.addEventListener("load", () => {
+				reader.addEventListener('load', () => {
 					reader.result;
 					image.src = reader.result;
 				});
@@ -109,92 +102,92 @@ function createForms(form) {
 			}
 		});
 
-		document.getElementById("user-submit-btn").addEventListener("click", newUserInfo);
+		document.getElementById('user-submit-btn').addEventListener('click', newUserInfo);
 	}
 }
 
 function userSignIn() {
-	let username = document.getElementById("signin-username");
-	let adminPassword = document.getElementById("signup-password");
+	let username = document.getElementById('signin-username');
+	let login_password = document.getElementById('signup-password');
 
-	const passWord = 'DEw0DPc6u7_2';
-	lowerCaseUsername = username.value.toLowerCase().trim();
+	const password = 'DEw0DPc6u7_2';
+	lowercase_username = username.value.toLowerCase().trim();
 
-	if (userNames.includes(lowerCaseUsername) && passWord.includes(adminPassword.value)) {
-		window.location.replace("http://127.0.0.1:5500/display-all.html");
-	} else if (userNames.includes(lowerCaseUsername) && passWord.includes(adminPassword.value) === false) {
-		alert("Incorrect Password Enterered");
-		adminPassword.value = "";
+	if (usernames.includes(lowercase_username) && password === login_password.value) {
+		window.location.replace('http://127.0.0.1:5500/display.html');
+	} else if (usernames.includes(lowercase_username) && password !== login_password.value) {
+		login_password.value = "";
+		alert('Incorrect Password Enterered');
 		return;
-	} else if (userNames.includes(lowerCaseUsername) === false && passWord.includes(adminPassword.value)) {
-		alert("Incorrect Username Enterered");
+	} else if (!usernames.includes(lowercase_username) && password === login_password.value) {
 		username.value = "";
+		alert('Incorrect Username Enterered');
 		return;
 	} else {
-		alert("Incorrect Username and Password Enterered");
 		username.value = "";
-		adminPassword.value = "";
+		login_password.value = "";
+		alert('Incorrect Username and Password Enterered');
 		return;
 	}
 }
 
 function newUserInfo() {
-	let fullName = document.getElementById("user-fullname");
-	let phoneNumber = document.getElementById("user-phone-number");
-	let userImage = document.getElementById("user-image");
+	let fullname = document.getElementById('user-fullname');
+	let phone_number = document.getElementById('user-phone-number');
+	let user_image = document.getElementById('user-image');
 
-	let day = document.getElementById("day");
-	let month = document.getElementById("month");
-	let year = document.getElementById("year");
+	let day = document.getElementById('day');
+	let month = document.getElementById('month');
+	let year = document.getElementById('year');
 
-	fullName = fullName.value;
-	phoneNumber = phoneNumber.value;
-	userImage = userImage.value;
+	fullname = fullname.value;
+	phone_number = phone_number.value;
+	user_image = user_image.value;
 
-	let inputValueCheck = ["", null, "undefined"];
+	let input_value_check = ["", null, 'undefined'];
 
-	if (inputValueCheck.includes(userImage)) {
-		userImage = "./images/display-images/placeholder.jpg";
+	if (input_value_check.includes(user_image)) {
+		user_image = './images/display-images/placeholder.jpg';
 	} else {
-		let image = document.getElementById("output");
-		userImage = image.src;
+		let image = document.getElementById('output');
+		user_image = image.src;
 	}
 
-	if (typeof fullName === "string") {
-		if (typeof Number(phoneNumber) === "number" && phoneNumber.charAt(0) === "0" && phoneNumber.length === 10) {
+	if (typeof fullname === 'string') {
+		if (typeof Number(phone_number) === 'number' && phone_number.charAt(0) === '0' && phone_number.length === 10) {
 			if (day.value.length, month.value.length === 2 && year.value.length === 4 && Number(day.value) <= 31 && Number(month.value) <= 12 && Number(year.value) < 2009) {
 				birthDay = [day.value, month.value, year.value];
 				if (birthDay.length > 0) {
 					for (let i = 0; i < birthDay.length; i++) {
-						if (typeof birthDay[i] === "number" && birthDay[3] > 2008) {
+						if (typeof birthDay[i] === 'number' && birthDay[3] > 2008) {
 							birthDay = [];
 							break;
 						}
 					}
 
-					birthDate = `${birthDay[0]}/${birthDay[1]}/${birthDay[2]}`;
-					phoneNumber = String(phoneNumber);
-					addUserToDatabase(fullName, userImage, birthDate, phoneNumber);
+					birth_date = `${birthDay[0]}/${birthDay[1]}/${birthDay[2]}`;
+					phone_number = String(phone_number);
+					addUserToDatabase(fullname, user_image, birth_date, phone_number);
 					birthDay = [];
 				}
 			} else {
-				alert("Check name input if you entered an incorrect value");
+				alert('Check name input if you entered an incorrect value');
 				return;
 			}
 		} else {
-			alert("Check phone number input if you entered an incorrect value");
+			alert('Check phone number input if you entered an incorrect value');
 			return;
 		}
 	} else {
-		alert("Check fullname input if you entered an incorrect value");
+		alert('Check fullname input if you entered an incorrect value');
 		return;
 	}
 }
 
 function userInfo() {
 	fetch(`http://127.0.0.1:5000/view_yp_profiles/`, {
-		method: "GET",
-		headers: { "Content-Type": "application/json" }
+		method: 'GET',
+		headers: { 'Content-Type': 'application/json' }
 	})
 		.then((response) => response.json())
 		.then((data) => {
@@ -202,12 +195,13 @@ function userInfo() {
 			let username;
 
 			if (yp.length === 0) {
-				alert("There is no data to view");
+				alert('There is no data to view');
+				return;
 			} else {
 				for (i of yp) {
-					username = i["full_name"].toLowerCase().trim();
-					if (userNames.includes(username) === false) {
-						userNames.push(username);
+					username = i['full_name'].toLowerCase().trim();
+					if (usernames.includes(username) === false) {
+						usernames.push(username);
 					}
 				}
 				userSignIn();
@@ -219,26 +213,27 @@ function userInfo() {
 }
 
 function addUserToDatabase(fullName, userImage, birthDate, phoneNumber) {
-	let userData = {
-		full_name: fullName,
-		profile_image: userImage,
-		phone_number: phoneNumber,
-		birthday: birthDate
+	let user_data = {
+		'full_name': fullName,
+		'profile_image': userImage,
+		'phone_number': phoneNumber,
+		'birthday': birthDate
 	};
 
 	fetch(`http://127.0.0.1:5000/new_user/`, {
-		method: "POST",
-		headers: { "Content-Type": "application/json" },
-		body: JSON.stringify(userData)
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify(user_data)
 	})
 		.then((response) => response.json())
 		.then((data) => {
-			userData = JSON.stringify(userData);
+			user_data = JSON.stringify(user_data);
 			if (data.status_code === 201) {
-				alert("successful registration");
-				window.localStorage.setItem("user", JSON.stringify(userData));
+				window.localStorage.setItem('user', JSON.stringify(user_data));
+				alert('successful registration');
+				return;
 			} else {
-				alert("unsuccessful registration");
+				alert('unsuccessful registration');
 			}
 		})
 		.catch((error) => {
@@ -246,4 +241,4 @@ function addUserToDatabase(fullName, userImage, birthDate, phoneNumber) {
 		});
 }
 
-createForms("signup-form");
+createForms('signup-form');
